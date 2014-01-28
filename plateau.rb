eb5df63f8deaef023549cf94e57805d8b004e04e
ryanhburbank <ryanhburbank@gmail.com>
2014-01-28 01:05:52 -0800

    add grid display method

--- plateau.rb
+++ plateau.rb
@@ -10,7 +10,25 @@ class Plateau
 
 	def generate_grid
 		grid = []
-		(1..(rows * columns)).to_a.each_slice(rows) {|row| grid << row }
+		(1..(rows * columns)).to_a.each_slice(columns) {|column| grid << column }
 		grid
 	end
+
+	def grid_display
+		display_grid = grid.reverse
+		longest = 0 
+		display_grid.each {|row| row.each {|e| longest = e.to_s.length if longest < e.to_s.length  }}
+		display = ""
+		display_grid.each do |row|
+			row_string = ""
+			row.each do |coord|
+				formatted_coordinate =  " " * (longest - coord.to_s.length) + coord.to_s
+				row_string << "|#{formatted_coordinate}|" 
+			end
+			display << row_string + "\n"
+		end
+		puts display
+	end
 end
+
+foo = Plateau.new({rows: 10, columns: 12})
