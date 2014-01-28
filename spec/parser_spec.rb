aeee2c96fc68187ab3e224201d3dad016aade6a7
ryanhburbank <ryanhburbank@gmail.com>
2014-01-27 20:01:19 -0800

    move sample & empty files to lib directory, update spec to match now paths

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -6,40 +6,40 @@ describe "Parser" do
 		let (:Parser) { Parser }
 
 		it "takes a .txt file and returns a properly formatted hash " do
-			Parser.parse("sample_input.txt").should eq correct_output
+			Parser.parse("lib/sample_input.txt").should eq correct_output
 		end
 
 		it "takes a .csv file and returns an Array " do
-			Parser.parse("sample_input.csv").should eq correct_output
+			Parser.parse("lib/sample_input.csv").should eq correct_output
 		end
 
 		it "takes a .yml file and returns an Array " do
-			Parser.parse("sample_input.yml").should eq correct_output
+			Parser.parse("lib/sample_input.yml").should eq correct_output
 		end
 
 		context "the passed file is empty & the filetype is supported" do
 			
 			it "raises an error if it is a csv file" do
-				lambda { Parser.parse("empty_input.csv") }.should raise_error(ArgumentError)
+				lambda { Parser.parse("lib/empty_input.csv") }.should raise_error(ArgumentError)
 			end
 			
 			it "raises an error if it is a yml file" do
-				lambda { Parser.parse("empty_input.yml") }.should raise_error(ArgumentError)
+				lambda { Parser.parse("lib/empty_input.yml") }.should raise_error(ArgumentError)
 			end
 			
 			it "raises an error if it a txt file" do	
-				lambda { Parser.parse("empty_input.txt") }.should raise_error(ArgumentError)
+				lambda { Parser.parse("lib/empty_input.txt") }.should raise_error(ArgumentError)
 			end
 		end
 
 		context "the passed file's extension is unsupported" do
 			
 			it "raises an error if file has contents" do
-				lambda { Parser.parse("empty_input.foo")}.should raise_error(ArgumentError)
+				lambda { Parser.parse("lib/empty_input.foo")}.should raise_error(ArgumentError)
 			end
 
 			it "raises an error if file is empty" do
-				lambda { Parser.parse("sample_input.foo")}.should raise_error(ArgumentError)
+				lambda { Parser.parse("lib/sample_input.foo")}.should raise_error(ArgumentError)
 			end
 		end
 	end
