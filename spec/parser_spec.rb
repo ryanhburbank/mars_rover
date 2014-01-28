d0498c91745cf69db547cc93032c985934be7b5e
ryanhburbank <ryanhburbank@gmail.com>
2014-01-27 17:44:25 -0800

    rename navigation class to instruction, add sample files in csv, yml, txt formats

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,2 +1,23 @@
 require 'spec_helper'
 
+describe "Parser" do
+	describe "#self.parse" do
+		it "takes a .txt file and returns an Array " do
+			pending
+		end
+
+		it "takes a .csv file and returns an Array " do
+			pending
+		end
+
+		it "takes a .yml file and returns an Array " do
+			pending
+		end
+
+		it "raises an error if the passed file is empty" do
+			Parser.parse("empty_file_csv.csv").should eq []
+			Parse.parse("empty_file_yaml.yml").should eq []
+			Parse.parse("empty_file_txt.txt").should eq []
+		end
+	end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,7 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require
 
-require_relative '../navigation'
+require_relative '../instruction'
 require_relative '../parser'
 require_relative '../plateau'
 require_relative '../rover'
-require_relative '../sample_input'
