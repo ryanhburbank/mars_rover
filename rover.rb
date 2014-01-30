4726d34fc375230a6e3999092d601518b93379db
ryanhburbank <ryanhburbank@gmail.com>
2014-01-30 00:40:58 -0800

    add methods for turning left, right, and moving

--- rover.rb
+++ rover.rb
@@ -1,9 +1,56 @@
+require 'fiber'
 
 class Rover
-	attr_reader :orders
-	HEADINGS = { n:[0,1], e: [1,0], s: [0,-1], w:[-1,0] }
-
+	attr_reader :position, :current_heading
+	MOVE_VALUES  = { "n" => [0,1], "e" => [1,0], "s" => [0,-1], "w" => [-1,0] }
+	DIRECTIONS = [ "n", "e", "s", "w" ]
+	
 	def initialize(orders)
-		@orders = orders
+		@position         = orders[:position]
+		@moves            = orders[:moves].each
+		@fiber            = set_fiber
+		@current_heading  = @fiber.resume
+	end
+
+	def move!
+		begin
+			current_move = @moves.next
+			 self.send(current_move)
+			 @position
+		rescue
+			return "ALERT: NO MOVES REMAINING!"
+		end
+	end
+
+	def moves
+		@moves.to_a
+	end
+
+	private
+	def l
+		2.times { |int| @fiber.resume }
+		@current_heading = @fiber.resume
+		@position[:heading] = @current_heading
+	end
+
+	def r
+		@current_heading = @fiber.resume
+		@position[:heading] = @current_heading
+	end
+
+	def m
+		values = MOVE_VALUES[@current_heading]
+		@position[:x] += values[0]
+		@position[:y] += values[1]
+	end
+
+	def set_fiber
+		Fiber.new do 
+			iterator = DIRECTIONS.cycle
+			DIRECTIONS.index(@position[:heading]).times {|int| iterator.next }
+			while iterator
+				Fiber.yield iterator.next
+			end
+		end
 	end
 end
--- spec/rover_spec.rb
+++ spec/rover_spec.rb
@@ -1,17 +1,37 @@
 require 'spec_helper'
 
 describe "Rover" do 
+	let (:instructions) { Instruction.new("lib/sample_input.txt") }
+	let (:rover) { Rover.new(instructions.rovers[:rover1]) }
+	
 	describe "#new" do
-		let (:instructions) { Instruction.new("lib/sample_input.txt") }
-		let (:rover) { Rover.new(instructions.rovers[:rover1]) }
 		
 		it "takes a hash and returns a Rover object" do
 			rover.should be_an_instance_of Rover
 		end
 
-		it "sets @orders to the hash's value" do
-			rover.orders.should eq ({ position: { x: 1, y: 2, heading: "N" }, 
-															 moves: ["l","m","l","m","l","m","l","m","m"] })
+		it "sets @position to the hash's :position key" do
+			rover.position.should eq ({ x: 1, y: 2, heading: "n"})
+		end
+
+		it "sets @moves to the hash's :moves key" do
+			rover.moves.should eq (["l","m","l","m","l","m","l","m","m"])
+		end
+	end
+
+	describe "#move!" do
+		context "there are elements remaining in the moves array" do
+			it "updates the rover's position or heading by one move" do
+				rover.position.should eq ({ x: 1, y: 2, heading: "n" })
+				rover.move!
+				rover.position.should eq ({ x: 1, y: 2, heading: "w" })
+			end
+		end
+		context "there are no elements remaining in the moves array" do
+			it "returns 'ALERT: NO MOVES REMAINING!' when called" do
+				rover.moves.count.times {|x| rover.move! }
+				rover.move!.should eq "ALERT: NO MOVES REMAINING!"
+			end 
 		end
 	end
 end
