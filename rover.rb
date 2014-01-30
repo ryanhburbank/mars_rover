cb3a48fb4783cca3fb9ef98eb3fe900a7e99e6b3
ryanhburbank <ryanhburbank@gmail.com>
2014-01-30 01:38:11 -0800

    refactor rover class, all tests pass

--- rover.rb
+++ rover.rb
@@ -5,10 +5,10 @@ class Rover
 	MOVE_VALUES  = { "n" => [0,1], "e" => [1,0], "s" => [0,-1], "w" => [-1,0] }
 	DIRECTIONS = [ "n", "e", "s", "w" ]
 	def initialize(orders)
-		@position         = orders[:position]
-		@moves            = orders[:moves].each
-		@fiber            = set_fiber
-		@current_heading  = @fiber.resume
+		@position            = orders[:position]
+		@moves               = orders[:moves].each
+		@fiber               = set_fiber
+		@position[:heading]  = @fiber.resume
 	end
 
 	def move_once!
@@ -21,12 +21,8 @@ class Rover
 		end
 	end
 
-	def move_count
-		puts @moves.count
-	end
-
 	def move_all!
-		(@moves.to_a.length).times { move_once! }
+		moves.length.times { move_once! }
 		@position
 	end
 
@@ -37,17 +33,15 @@ class Rover
 	private
 	def l
 		2.times { |int| @fiber.resume }
-		@current_heading = @fiber.resume
-		@position[:heading] = @current_heading
+		@position[:heading] = @fiber.resume
 	end
 
 	def r
-		@current_heading = @fiber.resume
-		@position[:heading] = @current_heading
+		@position[:heading] = @fiber.resume
 	end
 
 	def m
-		values = MOVE_VALUES[@current_heading]
+		values = MOVE_VALUES[@position[:heading]]
 		@position[:x] += values[0]
 		@position[:y] += values[1]
 	end
