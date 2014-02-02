2bb39e36002fe3bbc3a1b1c0d797602ef6302e0b
ryanhburbank <ryanhburbank@gmail.com>
2014-02-02 00:14:53 -0600

    refactor interface

--- interface.rb
+++ interface.rb
@@ -1,18 +1,22 @@
 Dir["./app/*.rb"].each { |file| require file }
 
-if ARGV[0]
-	mission = Mission.new(ARGV[0])
-else
-	mission = Mission.new("./lib/sample_input2.txt")
+class Interface
+
+	def self.run
+			mission = Mission.new(ARGV[0]) if ARGV[0]
+			mission ||= Mission.new("./lib/sample_input2.txt")
+			move_rovers(mission.rovers)
+	end
+
+	private
+	def self.move_rovers(rover_orders)
+		final_positions = ""
+		rover_orders.each_value do |orders|
+			rover = Rover.new(orders)
+			final_positions << "#{rover.move_all!} \n"
+		end
+		final_positions
+	end
 end
 
-rover_positions = []
-
-mission.rovers.each_value do |rover_order|
-	rover = Rover.new(rover_order)
-	rover.move_all!
-	puts rover.position
-end
-
-
-
+puts Interface.run
